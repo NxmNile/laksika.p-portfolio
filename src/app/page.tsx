20e6a8b16c6b5e0f42eb094cfe9a2b42da101f71
Laksika <laksikapanich@gmail.com>
2025-06-29 20:30:30 +0700

    experience part

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,57 @@
 import React from "react";
 import Navbar from "../components/navbar";
+import Timeline from "../components/timeline";
 
 export default function Home() {
   return (
     <>
-      <div className="bg-[#242424] min-h-screen w-full flex flex-col items-center">
-        <div className="flex items-center justify-center w-full mt-5">
-          <Navbar />
-        </div>
-
-        <div className="flex flex-col md:flex-row items-center justify-center gap-8 mt-14 p-4 w-[858px] ">
-          <div>
-            <img src="/image/profilepic.png" alt="Profile" />
+      <div className="relative">
+        <div className="bg-[#242424] min-h-screen w-full flex flex-col items-center">
+          <div className="flex items-center justify-center w-full mt-10">
+            <Navbar />
           </div>
 
-          <div className="w-full md:w-2/3 text-white ml-10 font-semibold">
-            <h2 className="text-4xl mb-4">Hello!</h2>
-            <ul className="list-disc list-inside space-y-4 text-2xl">
-              <div>
-                I'm <span className="text-[#A781FF]">Laksika Panich</span>,
-              </div>
-              <div>Software Engineer</div>
-              <button className="btn bg-[#20D8E5] hover:bg-[#19B2BF] border-none h-12 w-40 text-black font-semibold rounded-3xl mt-4">
-                Resume
-              </button>
-            </ul>
+          <div className="flex flex-col md:flex-row items-center justify-center gap-8 mt-14 p-4 w-[858px] relative ">
+            <div>
+              <img src="/image/profilepic.png" alt="Profile" />
+            </div>
+
+            <div className="w-full md:w-2/3 text-white ml-10 font-semibold">
+              <h2 className="text-4xl mb-4">Hello!</h2>
+              <ul className="list-disc list-inside space-y-4 text-2xl">
+                <div>
+                  I'm <span className="text-[#A781FF]">Laksika Panich</span>,
+                </div>
+                <div>Software Engineer</div>
+                <button className="btn bg-[#20D8E5] hover:bg-[#19B2BF] border-none h-12 w-40 text-black font-semibold rounded-3xl mt-4">
+                  Resume
+                </button>
+              </ul>
+            </div>
+          </div>
+          <div className="mt-10 text-white text-left w-[858px]">
+            I'm a developer who loves building fun, functional, and
+            user-centered digital experiences. From modern websites to mobile
+            apps and playful games, I enjoy solving problems through creativity
+            and code. I'm always learning and excited to take on new challenges
+            that push my skills further.
+          </div>
+          <div className="w-[858px] mt-20">
+            <h1 className="text-white text-3xl font-bold mt-10">
+              Work Experince
+            </h1>
+            <div className="realtive">
+              <img
+                src="/image/star1.png"
+                className="absolute top-220  left-335 w-40 h-40"
+                alt="Star"
+              />
+              <Timeline />
+            </div>
           </div>
         </div>
-        <div className="mt-10 text-white text-left w-[858px]">
-          I'm a developer who loves building fun, functional, and user-centered
-          digital experiences. From modern websites to mobile apps and playful
-          games, I enjoy solving problems through creativity and code. I'm
-          always learning and excited to take on new challenges that push my
-          skills further.
-        </div>
-        
+        <div className="absolute w-100 h-100 rounded-full bg-[#813fcc] opacity-40 blur-3xl pointer-events-none top-100 -left-10"></div>
+
       </div>
     </>
   );
---
+++ src/components/timeline.tsx
@@ -0,0 +1,102 @@
+import React from "react";
+
+const Timeline = () => {
+  return (
+    <div className="p-8 max-w-sm  mt-10">
+      <ol className="relative w-[858px] items-center">
+        {/* --- Step 1: Completed --- */}
+        <li className="mb-8 ms-20 ">
+          {/* เส้นเชื่อมสีฟ้า */}
+          <div className="absolute w-1 h-140 mt-20 bg-[#20D8E5] rounded-full -start-[0.8rem] "></div>
+
+          {/* วงกลมและไอคอน */}
+          <span className="absolute flex items-center justify-center w-10 h-10 mt-20 bg-purple-100 rounded-full -start-8 "></span>
+
+          <div className="bg-[#CFCFCF]/25 rounded-3xl  p-6 border-2 border-white/50 backdrop-blur-xs">
+            <h2 className="text-lg font-semibold mb-4">
+              January 2025 - Present
+            </h2>
+            <p className="font-semibold text-sm">
+              <span className="text-[#A781FF]">Research Assistant </span>at
+              HITAP, Thailand
+            </p>
+            <p className="text-sm mt-1">
+              Development in multiple projects involves various roles, including
+              Web Developer (Front-end), UX/UI designer for healthcare website
+              and Game Developer for developing game that providing knowledge
+            </p>
+          </div>
+        </li>
+
+        {/* --- Step 2: Completed --- */}
+        <li className="mb-8 ms-20">
+          
+
+          {/* วงกลมและไอคอน */}
+          <span className="absolute flex items-center justify-center w-10 h-10 mt-15 bg-purple-100 rounded-full -start-8 "></span>
+
+          <div className="bg-[#CFCFCF]/25 rounded-3xl p-6 border-2 border-white/50 backdrop-blur-xs">
+            <h2 className="text-lg font-semibold mb-4">
+              August 2024 - December 2024
+            </h2>
+            <p className="font-semibold text-sm">
+              <span className="text-[#A781FF]">Internship </span>at HITAP,
+              Thailand
+            </p>
+            <p className="text-sm mt-1">
+              Development in multiple projects involves various roles, including
+              Web Developer (Front-end) and UX/UI designer for healthcare
+              website.
+            </p>
+          </div>
+        </li>
+
+        {/* --- Step 3: Current --- */}
+        <li className="mb-8 ms-20">
+         
+
+          {/* วงกลมและไอคอน */}
+          <span className="absolute flex items-center justify-center w-10 h-10 mt-15 bg-white rounded-full -start-8 "></span>
+
+          <div className="bg-[#CFCFCF]/25 rounded-3xl p-6 border-2 border-white/50 backdrop-blur-xs">
+            <h2 className="text-lg font-semibold mb-4">May 2024 - July 2024</h2>
+            <p className="font-semibold text-sm">
+              <span className="text-[#A781FF] ">Internship </span>at Kanagawa
+              Institute of Technology,{" "}
+              <span className="text-[#20D8E5]">Japan</span>
+            </p>
+            <p className="text-sm mt-1">
+              Solo Game Developer using Unity and Godot to create 3D and Role
+              play game. Reseacrch aboout porting game from Unity to Godot
+            </p>
+          </div>
+        </li>
+
+        {/* --- Step 4: Upcoming (Last) --- */}
+        <li className="ms-20">
+          {/* วงกลมและไอคอน */}
+          <span className="absolute flex items-center justify-center w-10 h-10 mt-15 bg-white  rounded-full -start-8 ">
+            
+          </span>
+
+          <div className="bg-[#CFCFCF]/25 rounded-3xl p-6 border-2 border-white/50 backdrop-blur-xs">
+            <h2 className="text-lg font-semibold mb-4">
+              August 2023 - December 2023
+            </h2>
+            <p className="font-semibold text-sm">
+              <span className="text-[#A781FF]">Research Assistant </span>at
+              Center of Specialty Innovation (Cosi), Bangkok University,
+              Thailand
+            </p>
+            <p className="text-sm mt-1">
+              Developing Games providing knowledge about disease prevention for
+              Department of Disease Control
+            </p>
+          </div>
+        </li>
+      </ol>
+    </div>
+  );
+};
+
+export default Timeline;
